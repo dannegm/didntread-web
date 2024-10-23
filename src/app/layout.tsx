797b71aff48627fb3a88bdc8f508657e297fe232
dannegm <im@danielgarcia.me>
2024-10-23 11:30:58 -0600

    experiment with gitguardian

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { type ChildrenContainer } from '@/types/common';
 import { getEnvironmentInfo } from '@/helpers/environment';
 import RootLayout from '@/components/layout/root-layout';
 
+// Random Token, this is useless, just to see if gitguardian is catching
+// bqKPaAI3cF7azgTSvq6aiYCaQrqCZM9i
+
 const environmentInfo = getEnvironmentInfo();
 
 export const metadata: Metadata = {
